f0d36219263584c0fb5cd43b684b35cc05ba9951
Kosta Novokmet <nk200640d@student.etf.bg.ac.rs>
2024-12-20 17:25:54 +0100

    working const declaration + type inference for var decl

--- test/com/kosta/pp1/MJParserTest.java
+++ test/com/kosta/pp1/MJParserTest.java
@@ -8,12 +8,16 @@ import java.io.IOException;
 import java.io.Reader;
 
 import java_cup.runtime.Symbol;
+import rs.etf.pp1.symboltable.Tab;
+import rs.etf.pp1.symboltable.concepts.Obj;
+import rs.etf.pp1.symboltable.concepts.Struct;
 import java_cup.runtime.Scanner;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 
 import com.kosta.pp1.ast.Program;
+import com.kosta.pp1.ast.VisitorAdaptor;
 import com.kosta.pp1.utils.Log4JUtils;
 
 public class MJParserTest {
@@ -38,19 +42,22 @@ public class MJParserTest {
 			MJParser p = new MJParser(lexer);
 
 	        Symbol s = p.parse();  //pocetak parsiranja
-								   //
+			Tab.init();
+			Struct bool = new Struct(Struct.Bool);
+			Tab.currentScope.addToLocals(new Obj(Obj.Type, "bool", bool));
+
 	        Program prog = (Program)(s.value); 
 			// ispis sintaksnog stabla
 			log.info(prog.toString(""));
 			log.info("===================================");
 
 			// ispis prepoznatih programskih konstrukcija
-			RuleVisitor v = new RuleVisitor();
+			VisitorAdaptor v = new SemanticAnalyzer();
 			prog.traverseBottomUp(v); 
 	      
-			log.info(" Print count calls = " + v.printCallCount);
+			//log.info(" Print count calls = " + v.printCallCount);
 
-			log.info(" Deklarisanih promenljivih ima = " + v.varDeclCount);
+			//log.info(" Deklarisanih promenljivih ima = " + v.varDeclCount);
 			
 		} 
 		finally {
